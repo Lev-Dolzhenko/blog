a57b96b4fae5044a691860926f0574ed565d75b4
Leo <workspace2707@yandex.ru>
2023-12-06 12:38:52 +0300

    create edit function for cards of posts

--- js/main.js
+++ js/main.js
@@ -20,29 +20,19 @@ const createPostButton = document.getElementById('createPost');
 
 const listPosts = document.querySelector('.posts__row ul');
 
-createPostButton.addEventListener('click', function(event) {
+let editButtonAll;
+
+createPostButton.addEventListener('click', function (event) {
     event.preventDefault();
     createPost(inputTitle.value, inputDescription.value);
+    editButtonAll = document.querySelectorAll('.posts__row ul li button');
+    console.log(editButtonAll)
 })
-
 function createPost(title, description) {
     listPosts.innerHTML +=
         `
-    <li>
-        <button>
-            <?xml version="1.0" encoding="utf-8"?><!-- Uploaded to: SVG Repo, www.svgrepo.com, Generator: SVG Repo Mixer Tools -->
-            <svg width="24px" height="24px" viewBox="0 0 24 24" fill="none"
-            xmlns="http://www.w3.org/2000/svg">
-            <path
-            d="M21.2799 6.40005L11.7399 15.94C10.7899 16.89 7.96987 17.33 7.33987 16.7C6.70987 16.07 7.13987 13.25 8.08987 12.3L17.6399 2.75002C17.8754 2.49308 18.1605 2.28654 18.4781 2.14284C18.7956 1.99914 19.139 1.92124 19.4875 1.9139C19.8359 1.90657 20.1823 1.96991 20.5056 2.10012C20.8289 2.23033 21.1225 2.42473 21.3686 2.67153C21.6147 2.91833 21.8083 3.21243 21.9376 3.53609C22.0669 3.85976 22.1294 4.20626 22.1211 4.55471C22.1128 4.90316 22.0339 5.24635 21.8894 5.5635C21.7448 5.88065 21.5375 6.16524 21.2799 6.40005V6.40005Z"
-            stroke="#fff" stroke-width="1.5" stroke-linecap="round"
-            stroke-linejoin="round" />
-            <path
-            d="M11 4H6C4.93913 4 3.92178 4.42142 3.17163 5.17157C2.42149 5.92172 2 6.93913 2 8V18C2 19.0609 2.42149 20.0783 3.17163 20.8284C3.92178 21.5786 4.93913 22 6 22H17C19.21 22 20 20.2 20 18V13"
-            stroke="#fff" stroke-width="1.5" stroke-linecap="round"
-            stroke-linejoin="round" />
-            </svg>
-        </button>
+        <li>
+        <h5>EDIT</h5>
         <img src="/img/posts/postOne.jpg" alt="First post">
         <div class="post__info">
             <h4>${title}</h4>
@@ -73,6 +63,29 @@ function time() {
 
 /* EDIT POST */
 
+let editMode = false;
+
+listPosts.addEventListener('click', function (event) {
+    const postItem = event.target.closest('li');
+    const title = postItem.querySelector('h4');
+    const description = postItem.querySelector('p');
+    const buttonFlag = postItem.querySelector('h5')
+
+    if (event.target.tagName === 'H5') {
+
+        if (editMode) {
+            title.removeAttribute('contentEditable');
+            description.removeAttribute('contentEditable');
+            buttonFlag.textContent = 'EDIT';
+        }
+        else {
+            title.setAttribute('contentEditable', true);
+            description.setAttribute('contentEditable', true);
+            buttonFlag.textContent = 'SAVE';
+        }
+    }
+    editMode = !editMode;
+});
 
 
 /* EDIT POST */
